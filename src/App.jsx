50ae6d0daff32e85709bd337b3aaf4fd296d9281
lengtith <sotith1998@gmail.com>
2024-03-13 22:40:08 +0700

    load-more-button

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Accordian from "./components/accordian"
 import ImageSlider from "./components/image-slider";
+import LoadMoreData from "./components/load-more-button";
 import RandomColor from './components/random-color';
 import StarRating from "./components/star-rating";
 
@@ -11,6 +12,7 @@ function App() {
       <RandomColor />
       <StarRating noOfStars={5} />
       <ImageSlider />
+      <LoadMoreData/>
     </>
   )
 }
